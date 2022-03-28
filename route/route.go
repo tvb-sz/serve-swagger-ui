63d39efdf7bbe029d21cd4d560e87f0557ab56fd
杨晶晶 <jingjing.yang@tvb.com>
2022-03-28 20:06:31 +0800

    feat: append email field to logger if exist

--- route/route.go
+++ route/route.go
@@ -3,7 +3,9 @@ package route
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jjonline/go-lib-backend/logger"
+	"github.com/tvb-sz/serve-swagger-ui/app/service"
 	"github.com/tvb-sz/serve-swagger-ui/conf"
+	"go.uber.org/zap"
 )
 
 // router 包内路由变量，请勿覆盖
@@ -15,13 +17,24 @@ func iniRoute() {
 	router = gin.New()
 
 	// set base middleware
-	router.Use(logger.GinLogger(nil), logger.GinRecovery)
+	router.Use(logger.GinLogger(appendEmailIfExist), logger.GinRecovery)
 	if conf.Config.Server.Cors {
 		router.Use(logger.GinCors)
 	}
 
 	// 请求找不到路由时输出错误
 	router.NoRoute(notRoute)
+}
+
+// appendEmailIfExist append email field to logger if exist
+func appendEmailIfExist(ctx *gin.Context) []zap.Field {
+	if tokenInter, exist := ctx.Get("token"); exist {
+		if token, ok := tokenInter.(service.Token); ok && token.Authenticated {
+			filed := make([]zap.Field, 0)
+			return append(filed, zap.String("email", token.Email))
+		}
+	}
+	return nil
 }
 
 // Bootstrap 引导初始化路由route
